e496a33baf1ca23756d06ccb8249320678aec221
YJLEE <dldbwjd24@gmail.com>
2024-12-18 15:29:21 +0900

    feat : button figma연동
    feat : button figma연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,47 @@
 import Image from 'next/image';
+import Button from './ui-component/Button/Button';
 
 export default function Home() {
-  return <>Hello. Nest.js</>;
+  return <>
+    Hello. Nest.js
+
+
+<Button
+    type="Stock"
+    size="large"
+    stock="correction"
+    state="default"
+    leftIcon
+    rightIcon
+>
+    {"버튼명"}
+</Button>
+
+<Button
+    type="Stock"
+    size="medium"
+    stock="red"
+    state="default"
+    leftIcon
+    rightIcon
+>
+    {"버튼명"}
+</Button>
+
+
+<Button type="Tertiary" size="large" state="default" leftIcon rightIcon>
+    {"버튼명"}
+</Button>
+
+
+<Button type="Secondary" size="medium" state="pressed" rightIcon>
+    {"버튼명"}
+</Button>
+
+
+<Button type="Secondary" size="medium" state="pressed">
+    {"버튼명"}
+</Button>
+
+  </>;
 }
--- src/app/ui-component/Button/Button.tsx
+++ src/app/ui-component/Button/Button.tsx
@@ -1,22 +1,23 @@
 import { ReactEventHandler, ReactNode } from "react";
+import plus1 from '../../assets/img/icons/12/arrow/plus.png';
 
-type ButtonPropsType = "primary" | "scondary" | "tertiary" | "stock"
+type ButtonPropsType = "primary" | "Secondary" | "Tertiary" | "Stock" | "danger"
 type ButtonPropsSize = "large" | "medium" | "small" | "xsmall"
 type ButtonPropsStock = "blue" | "red" | "cancel" | "correction" | "disabled"
 type ButtonPropsState = "default" | "pressed" | "disabled"
 
-interface ButtonElementButtonTypeSubmitProps {
-    onClick: ReactEventHandler<HTMLButtonElement>;
-    type?:"submit";
-}
+// interface ButtonElementButtonTypeSubmitProps {
+//     onClick: ReactEventHandler<HTMLButtonElement>;
+//     type?:"submit";
+// }
 
-interface ButtonElementButtonTypeButtonProps {
-    onClick: ReactEventHandler<HTMLButtonElement>;
-    type?:"button";
-}
+// interface ButtonElementButtonTypeButtonProps {
+//     onClick: ReactEventHandler<HTMLButtonElement>;
+//     type?:"button";
+// }
 
-type ButtonElementProps = ButtonElementSharedProps &
-    ButtonElementButtonTypeButtonProps | ButtonElementButtonTypeSubmitProps
+type ButtonElementProps = ButtonElementSharedProps 
+// & ButtonElementButtonTypeButtonProps | ButtonElementButtonTypeSubmitProps
 
 export interface ButtonElementSharedProps {
 
@@ -24,7 +25,7 @@ export interface ButtonElementSharedProps {
      * 버튼의 타입을 명시하는 변수 
      * @default primary
      */
-    btnType? : ButtonPropsType;
+    type? : ButtonPropsType;
     /**
      * 버튼의 사이즈  
      * @default large
@@ -45,25 +46,94 @@ export interface ButtonElementSharedProps {
      * @default false
      */
     leftIcon? : boolean;
+    /**
+     * 왼쪽에 아이콘이 추가되어있는 버튼인지 여부
+     * @default false
+     */
+    rightIcon? : boolean;
+    /**
+     * React children
+     */
+    children: ReactNode;
+}
 
+
+function getImgSize(size:ButtonPropsSize){
+    let imgSize = 24
+    switch(size){
+        case "large": 
+            imgSize = 24
+            break
+        case "medium":
+            imgSize = 16
+            break
+        case "small":
+            imgSize = 12
+            break
+        case "xsmall":
+            imgSize = 12
+            break
+    }
+
+    return imgSize
 }
 
 export const Button = ({
-    btnType = "primary",
+    type = "primary",
     size = "large",
+    state = "default",
     ...props
 }:ButtonElementProps) => {
     const classNames = [
         "button",
         `button-size-${size}`,
-        `button-varient-${btnType}`
     ]
 
     const contents: ReactNode[] = []
 
-    return (
-        
+    if(type === 'Stock' && props.stock){
+        console.log("?stock")
+        classNames.push(`button-varient-${type}-${props.stock}`)
+    } else{
+        classNames.push(`button-varient-${type}`)
+    }
+
+    if(state === 'disabled'){
+        classNames.push('button-state-disabled')
+    }
+
+    //나중에 로직좀 수정할 것.. 일단 되는지 부터 보고
+    if(props.leftIcon || props.rightIcon){
+
+        if(props.leftIcon && props.rightIcon){
+            const imgFileL = process.env.NEXT_PUBLIC_TEST_URL + `/assets/img/icons/${getImgSize(size)}/arrow/plus.png`;
+            const imgFileR = process.env.NEXT_PUBLIC_TEST_URL +  `/assets/img/icons/${getImgSize(size)}/arrow/right.png`
+            contents.push(<img src={imgFileL}/>, <span>{props.children}</span>, <img src={imgFileR}/>)
+        }else{
+            if(props.leftIcon){
+                let imgFile = process.env.NEXT_PUBLIC_TEST_URL + `/assets/img/icons/${getImgSize(size)}/arrow/plus.png`
+                contents.push(<img src={imgFile}/>, <span>{props.children}</span>)
+            }
+            if(props.rightIcon){
+                let imgFile = process.env.NEXT_PUBLIC_TEST_URL + `/assets/img/icons/${getImgSize(size)}/arrow/right.png`
+                contents.push(<span>{props.children}</span>, <img src={imgFile}/>)
+            }
+        }
+
+    }else{
+        contents.push(props.children)
+    }
+
+
+    return(
+        <button
+            // aria-label="버튼명"
+            className={classNames.join(" ")}
+        >
+            {contents}
+        </button>
     )
 
-
 }
+
+export default Button;
